0c6f34634b53c19b87c6c577628a1b4db4571068
ShkalikovAV <shkalikov@atb.su>
2024-05-31 21:38:27 +0300

    test prod baseurl

--- src/taskpane/shared/api/instanceAxios.ts
+++ src/taskpane/shared/api/instanceAxios.ts
@@ -4,7 +4,9 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: process.env.NODE_ENV === "production" ? API_GATEWAY_DEFAULT : "",
   // baseURL: "http://ec2-13-53-249-255.eu-north-1.compute.amazonaws.com:8080",
-  baseURL: "",
+  // baseURL: "",
+  baseURL:
+    process.env.NODE_ENV === "production" ? "http://ec2-13-53-249-255.eu-north-1.compute.amazonaws.com:8080" : "",
 });
 
 export default instance;
