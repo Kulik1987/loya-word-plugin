20882e4410d68401d6b57c74fe368dfb5214f0c3
ShkalikovAV <shkalikov@atb.su>
2024-07-12 23:21:48 +0300

    anonymizer move to cmp

--- src/taskpane/pages/review/Review.tsx
+++ src/taskpane/pages/review/Review.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
-import { Button, Divider, Text } from "@fluentui/react-components";
+import { Divider, Text } from "@fluentui/react-components";
 import { useStores } from "../../store";
 import { ReviewTypeGeneral } from "./reviewTypeGeneral";
 import { ReviewTypeCustom } from "./reviewTypeCustom";
+import { Anonymizer } from "./anonymizer";
 import { PlayBook } from "./playBook";
-import { AddCircleRegular } from "@fluentui/react-icons";
 
 const T = {
   dividerSelectReviewType: {
@@ -27,30 +27,12 @@ const T = {
 };
 
 const Review = () => {
-  const { menuStore, documentStore } = useStores();
+  const { menuStore } = useStores();
   const { locale } = menuStore;
 
   useEffect(() => {
     console.log("navigate to [page review]");
   }, []);
-
-  const handleAddAnonymizedText = async () => {
-    documentStore.buildAnonymizedText();
-    const { textContractAnonymized } = documentStore;
-    if (typeof textContractAnonymized !== "string") return null;
-    await Word.run(async (context) => {
-      const body = context.document.body;
-      // Создаем новый Range в конце документа для прокрутки к концу
-      const range = body.getRange("End");
-      range.select();
-      // Добавляем текст в конец документа
-      body.insertText(textContractAnonymized, Word.InsertLocation.end);
-
-      await context.sync();
-    }).catch((error) => {
-      console.log("Error [handleAddAnonymizedText]: " + error);
-    });
-  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "32px" }}>
@@ -79,18 +61,7 @@ const Review = () => {
             {T.dividerAnonymizer[locale]}
           </Text>
         </Divider>
-        <Text size={300}>Опция добавляет в конец документа текст договора с удаленными персональными данными</Text>
-        <div>
-          <Button
-            appearance="primary"
-            size="medium"
-            style={{ whiteSpace: "nowrap" }}
-            icon={<AddCircleRegular />}
-            onClick={handleAddAnonymizedText}
-          >
-            {T.buttonAnonymizer[locale]}
-          </Button>
-        </div>
+        <Anonymizer />
       </div>
     </div>
   );
