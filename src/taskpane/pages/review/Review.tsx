9f07805334055cd32ada621c50583e43fc7bc19b
ShkalikovAV <shkalikov@atb.su>
2024-07-12 23:35:41 +0300

    anonymizer hide by env param

--- src/taskpane/pages/review/Review.tsx
+++ src/taskpane/pages/review/Review.tsx
@@ -7,6 +7,8 @@ import { ReviewTypeCustom } from "./reviewTypeCustom";
 import { Anonymizer } from "./anonymizer";
 import { PlayBook } from "./playBook";
 
+const APP_SET_ANONYMIZER = process.env.APP_SET_ANONYMIZER;
+
 const T = {
   dividerSelectReviewType: {
     ru: "Выберите тип проверки",
@@ -34,6 +36,7 @@ const Review = () => {
     console.log("navigate to [page review]");
   }, []);
 
+  const isDisplayAnonymizer = APP_SET_ANONYMIZER === "true";
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "32px" }}>
       <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
@@ -55,14 +58,16 @@ const Review = () => {
         <PlayBook />
       </div>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-        <Divider alignContent="center" inset>
-          <Text size={300} weight="medium">
-            {T.dividerAnonymizer[locale]}
-          </Text>
-        </Divider>
-        <Anonymizer />
-      </div>
+      {isDisplayAnonymizer && (
+        <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+          <Divider alignContent="center" inset>
+            <Text size={300} weight="medium">
+              {T.dividerAnonymizer[locale]}
+            </Text>
+          </Divider>
+          <Anonymizer />
+        </div>
+      )}
     </div>
   );
 };
